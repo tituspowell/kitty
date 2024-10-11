090122ef7fc9eff5d128ad893f89130ea262a39e
Titus Powell <tituspowell@gmail.com>
2024-10-11 11:48:35 +0100

    Added home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,61 @@
+import Image from 'next/image';
+import kittenImage from './images/KittyHome.png';
+import { theme } from './styles/theme';
+
 export default function Home() {
   return (
-    <div className='max-w-[600px] mx-auto'>
-      <h2>Home Page</h2>
+    <div className='max-w-[900px] mx-auto'>
+      <section className='max-w-[600px] mx-auto grid grid-cols-2 gap-4 justify-items-stretch align-top p-4 mb-4'>
+        {/* The title part */}
+        <div className='grid grid-cols-1 align-top justify-items-center h-[100px]'>
+          <h1
+            className={`${theme.text.lowContrast} text-6xl text-primary-500 italic`}
+          >
+            Kitty's
+          </h1>
+          <h1
+            className={`${theme.text.highContrast} text-2xl sm:text-3xl md:text-4xl mt-4`}
+          >
+            SUITE OF
+          </h1>
+          <h1
+            className={`${theme.text.highContrast} text-2xl sm:text-3xl md:text-4xl`}
+          >
+            <span className='italic'>REACT</span> DEMO
+          </h1>
+          <h1
+            className={`${theme.text.highContrast} text-2xl sm:text-3xl md:text-4xl`}
+          >
+            MINI-APPS
+          </h1>
+        </div>
+        <div className='relative group'>
+          {/* The image */}
+          <Image
+            src={kittenImage}
+            width={256}
+            height={256}
+            alt='kitten'
+            priority
+            className='shadow-lg'
+          ></Image>
+        </div>
+      </section>
+      {/* The card with info below */}
+      <div
+        className={`card ${theme.card} shadow-xl justify-center align-top p-4 m-4 mt-8`}
+      >
+        <h2 className='text-2xl font-bold mb-4'>Welcome!</h2>
+        <p className='text-lg'>
+          This set of three mini-apps is to showcase Titus Powell's web
+          development skills using <i>React</i>, <i>Next.js</i>,{' '}
+          <i>Typescript</i> and <i>Tailwind</i>.
+        </p>{' '}
+        <p className='text-lg mt-4'>
+          Each page has a <b>please explain</b> button if you want to know
+          what's happening behind the scenes.
+        </p>
+      </div>
     </div>
   );
 }
