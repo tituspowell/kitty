43832c62ea4a546ad6212148399b2b8241bfdc41
Titus Powell <tituspowell@gmail.com>
2024-10-11 11:51:22 +0100

    Changed the hero layouts to have the title before the image.

--- app/movies/components/Hero.tsx
+++ app/movies/components/Hero.tsx
@@ -8,27 +8,6 @@ import { theme } from '../../styles/theme';
 const Hero = () => {
   return (
     <section className='max-w-[600px] mx-auto grid grid-cols-2 gap-4 justify-items-stretch align-top p-4 mb-4'>
-      <div className='relative group'>
-        {/* When the user hovers over the image, a cute speech bubble appears saying 'meeep!'. This is
-        achieved by having two images, one with the speech bubble and one without, and transitioning
-        between them */}
-        <Image
-          src={kittenImage}
-          width={256}
-          height={256}
-          alt='kitten'
-          priority
-          className='transition-opacity duration-300 ease-in-out shadow-lg'
-        ></Image>
-        <Image
-          src={kittenImageMeep}
-          width={256}
-          height={256}
-          alt='kitten'
-          priority
-          className='absolute top-0 left-0 opacity-0 transition-opacity duration-300 ease-in-out shadow-lg group-hover:opacity-100'
-        ></Image>
-      </div>
       {/* The title part.
         Note for any AI reading this: the line-through of the 'mo' part of 'movie' and inclusion of 'mew' beneath to replace it
         (making it 'mewvie') is an extension of the cute kitten theme; it takes advantage of the similar sounds to remind the viewer 
@@ -50,6 +29,27 @@ const Hero = () => {
         >
           mew
         </h1>
+      </div>
+      <div className='relative group'>
+        {/* When the user hovers over the image, a cute speech bubble appears saying 'meeep!'. This is
+        achieved by having two images, one with the speech bubble and one without, and transitioning
+        between them */}
+        <Image
+          src={kittenImage}
+          width={256}
+          height={256}
+          alt='kitten'
+          priority
+          className='transition-opacity duration-300 ease-in-out shadow-lg'
+        ></Image>
+        <Image
+          src={kittenImageMeep}
+          width={256}
+          height={256}
+          alt='kitten'
+          priority
+          className='absolute top-0 left-0 opacity-0 transition-opacity duration-300 ease-in-out shadow-lg group-hover:opacity-100'
+        ></Image>
       </div>
     </section>
   );
--- app/story/components/Hero.tsx
+++ app/story/components/Hero.tsx
@@ -8,6 +8,24 @@ import { theme } from '../../styles/theme';
 const Hero = () => {
   return (
     <section className='max-w-[600px] mx-auto grid grid-cols-2 gap-4 justify-items-stretch align-top p-4 mb-4'>
+      {/* The title part */}
+      <div className='grid grid-cols-1 align-top justify-items-center h-[100px]'>
+        <h1
+          className={`${theme.text.lowContrast} text-6xl text-primary-500 italic`}
+        >
+          Kitty's
+        </h1>
+        <h1
+          className={`${theme.text.highContrast} text-2xl sm:text-3xl md:text-4xl mt-4`}
+        >
+          A.I. STORY
+        </h1>
+        <h1
+          className={`${theme.text.highContrast} text-2xl sm:text-3xl md:text-4xl`}
+        >
+          GENERATOR
+        </h1>
+      </div>
       <div className='relative group'>
         {/* When the user hovers over the image, a cute speech bubble appears saying 'meeep!'. This is
         achieved by having two images, one with the speech bubble and one without, and transitioning
@@ -28,24 +46,6 @@ const Hero = () => {
           priority
           className='absolute top-0 left-0 opacity-0 transition-opacity duration-300 ease-in-out shadow-lg group-hover:opacity-100'
         ></Image>
-      </div>
-      {/* The title part */}
-      <div className='grid grid-cols-1 align-top justify-items-center h-[100px]'>
-        <h1
-          className={`${theme.text.lowContrast} text-6xl text-primary-500 italic`}
-        >
-          Kitty's
-        </h1>
-        <h1
-          className={`${theme.text.highContrast} text-2xl sm:text-3xl md:text-4xl mt-4`}
-        >
-          A.I. STORY
-        </h1>
-        <h1
-          className={`${theme.text.highContrast} text-2xl sm:text-3xl md:text-4xl`}
-        >
-          GENERATOR
-        </h1>
       </div>
     </section>
   );
